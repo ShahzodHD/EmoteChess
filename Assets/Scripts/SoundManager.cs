43e6e1cb620afbef06e680470c015b8260734f26
Shahzod <85818921+ShahzodHD@users.noreply.github.com>
2022-04-12 20:02:47 +0700

    add sounds
    add all sound for pick figure

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -3,15 +3,26 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private AudioSource audioPawn;
+    [SerializeField] private AudioClip[] clipsPawn;
     [SerializeField] private AudioSource audioRock;
+    [SerializeField] private AudioClip[] clipsRock;
     [SerializeField] private AudioSource audioKnight;
+    [SerializeField] private AudioClip[] clipsKnight;
     [SerializeField] private AudioSource audioBishop;
     [SerializeField] private AudioSource audioQueen;
+    [SerializeField] private AudioClip[] clipsQueen;
     [SerializeField] private AudioSource audioKing;
+    [SerializeField] private AudioClip[] clipsKing;
+
+    private AudioClip _tempClipPawn;
+    private AudioClip _tempClipRock;
+    private AudioClip _tempClipKnight;
+    private AudioClip _tempClipQueen;
+    private AudioClip _tempClipKing;
     public void DefineTypeFigure(string name)
     {
         if (name.Contains("Pawn")) { PawnPlayClip(); }
-        if (name.Contains("Rock")) { RockPlayClip(); }
+        if (name.Contains("Rook")) { RookPlayClip(); }
         if (name.Contains("Knight")) { KnightPlayClip(); }
         if (name.Contains("Bishop")) { BishopPlayClip(); }
         if (name.Contains("Queen")) { QueenPlayClip(); }
@@ -19,32 +30,41 @@ public class SoundManager : MonoBehaviour
     }
     private void PawnPlayClip()
     {
-        print("œ¿¬Õ");
+        int index = Random.Range(0, 2);
+        _tempClipPawn = clipsPawn[index];
+        audioPawn.clip = _tempClipPawn;
         audioPawn.Play();
     }
-    private void RockPlayClip()
+    private void RookPlayClip()
     {
-        print("¡¿ÿÕﬂ");
+        int index = Random.Range(0, 2);
+        _tempClipRock = clipsRock[index];
+        audioRock.clip = _tempClipRock;
         audioRock.Play();
     }
     private void KnightPlayClip()
     {
-        print(" ŒÕ‹");
+        int index = Random.Range(0, 2);
+        _tempClipKnight = clipsKnight[index];
+        audioKnight.clip = _tempClipKnight;
         audioKnight.Play();
     }
     private void BishopPlayClip()
     {
-        print("—ÀŒÕ");
         audioBishop.Play();
     }
     private void QueenPlayClip()
     {
-        print("‘≈–«");
+        int index = Random.Range(0, 2);
+        _tempClipQueen = clipsQueen[index];
+        audioQueen.clip = _tempClipQueen;
         audioQueen.Play();
     }
     private void KingPlayClip()
     {
-        print(" Œ–ŒÀ‹");
+        int index = Random.Range(0, 2);
+        _tempClipKing = clipsKing[index];
+        audioKing.clip = _tempClipKing;
         audioKing.Play();
     }
 }
